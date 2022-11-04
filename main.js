9b94a8f7b6987ed98f99cf2ca6545459e1ec2330
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-04 21:40:53 +0000

    submit handler. local storage completed.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var data = {
+  view: 'home',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousDataJSON = localStorage.getItem('data-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+function unloadWindow(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
+window.addEventListener('beforeunload', unloadWindow);
+
 var $entryButton = document.querySelector('.add-entry-button');
 var $modal = document.querySelector('.modal');
 var $overlay = document.querySelector('.overlay');
@@ -13,4 +30,20 @@ $cancelButton.addEventListener('click', closeModal);
 function closeModal(event) {
   $modal.className = 'modal hidden';
   $overlay.className = 'overlay hidden';
+}
+
+var $submitForm = document.querySelector('.input-form');
+$submitForm.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
+  var obj = {};
+  event.preventDefault();
+  obj.dayofweek = $submitForm.elements.dayofweek.value;
+  obj.time = $submitForm.elements.time.value;
+  obj.notes = $submitForm.elements.notes.value;
+  obj.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(obj);
+  $submitForm.reset();
+  closeModal();
 }
